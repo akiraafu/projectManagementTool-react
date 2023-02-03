81bb25c8ab83a74bb7cf0c44d2815bddaee0cc4e
Junrong Fu <junrongfu6@gmail.com>
2023-02-03 22:40:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+/*pages
+
+- dashboard (homepage)
+- login
+- signup
+- create
+- project (project details)
+
+*/
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvRLVXrcuZCTPdZvt9wpDFjHf9KuuVfHU",
+  authDomain: "projectmanagementsite-d8a50.firebaseapp.com",
+  projectId: "projectmanagementsite-d8a50",
+  storageBucket: "projectmanagementsite-d8a50.appspot.com",
+  messagingSenderId: "269767423108",
+  appId: "1:269767423108:web:376d7f90df36a2d7e6fed1",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
