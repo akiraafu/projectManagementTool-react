89890115df44b29f14cb6933db3578bb555007bb
Junrong Fu <junrongfu6@gmail.com>
2023-02-04 16:01:22 +0800

    commit

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,85 @@
+import { useState } from "react";
+//styles
 import "./signup.css";
 
-import React from "react";
-
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 1000000) {
+      setThumbnailError("Image file size must be less than 100kb");
+      return;
+    }
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log("profile photo updated");
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Display name:</span>
+        <input
+          type="text"
+          required
+          onChange={(e) => {
+            setDisplayName(e.target.value);
+          }}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>Profile photo:</span>
+        <input type="file" required onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 };
 
 export default Signup;
