856bac1f26d66524ff976f475007dda1064a96a3
Junrong Fu <junrongfu6@gmail.com>
2023-02-06 21:05:51 +0800

    commit

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,9 +1,80 @@
+import { useState } from "react";
+import Select from "react-select";
 import "./create.css";
 
-import React from "react";
+const categories = [
+  {
+    value: "development",
+    label: "Development",
+  },
+  { value: "design", label: "Design" },
+  {
+    value: "sales",
+    label: "Sales",
+  },
+  { value: "marketing", label: "Marketing" },
+];
 
 const Create = () => {
-  return <div>Create</div>;
+  //form field values
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate, category.value);
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            type="text"
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          />
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            type="date"
+            required
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+
+        <label>
+          <span>Project Category:</span>
+          <Select
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* assignee select here */}
+        </label>
+        <button className="btn">Add Project</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
